0952d45e68c0bc9c1654929e42dce21d58322f43
Janath Gunarathna <95167638+JanathGunarathna@users.noreply.github.com>
2025-06-26 16:12:35 +0530

    added backend and frontend

---
+++ Backend/src/main/java/com/YummyGo/YummyGo/YummyGoApplication.java
@@ -0,0 +1,13 @@
+package com.YummyGo.YummyGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyGoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/YummyGo/YummyGo/YummyGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YummyGo.YummyGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
